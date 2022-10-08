542127c332cb661479234494c60d3a7c2bfb4d31
Igor Teplostanski <teplostanski@yandex.ru>
2022-10-08 21:32:36 +0300

    Исправляет появление ошибки 404 при перезагрузке страницы приложения развёрнутого на Github Pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './components/App/App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
