8e3c73cb3b79f24e9d4aeddeec2c34a017bb7a61
teplostanski <teplostanski@yandex.ru>
2023-03-20 23:19:17 +0300

    Добавляет новые проекты aned и portfolio

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -3,12 +3,32 @@ import howToLearn from '../images/how-to-learn.png';
 import movies from '../images/movies.png';
 import mesto from '../images/mesto.png';
 import travel from '../images/travel.png';
+import aned from '../images/aned.png';
+import portfolio from '../images/portfolio.png';
 
 const projects = [
   {
     id: uuidv4(),
+    name: 'aned',
+    techs: 'HTML CSS TypeScript React.js',
+    desc: 'Пет-проект. Онлайн редактор markdown-заметок',
+    img: aned,
+    site: 'https://aned.teplostanski.dev/',
+    source: 'https://github.com/teplostanski/aned',
+  },
+  {
+    id: uuidv4(),
+    name: 'Портфолио',
+    techs: 'HTML CSS BEM Adaptive React.js',
+    desc: 'Сайт-портфолио',
+    img: portfolio,
+    site: 'https://teplostanski.dev/',
+    source: 'https://github.com/teplostanski/portfolio',
+  },
+  {
+    id: uuidv4(),
     name: 'Movies Explorer',
-    techs: 'HTML CSS BEM Adaptive ReactJS',
+    techs: 'HTML CSS BEM Adaptive React.js',
     desc: 'Данное вeб-приложение является сайтом-портфолио и мини-кинопоиском, открывающимся после регистрации.',
     img: movies,
     site: '',
@@ -17,7 +37,7 @@ const projects = [
   {
     id: uuidv4(),
     name: 'Место',
-    techs: 'HTML CSS BEM Adaptive ReactJS',
+    techs: 'HTML CSS BEM Adaptive React.js',
     desc: '"Mesto" — это сайт, где люди обмениваются фотографиями. "Местом" может быть что угодно: город, область или какое-то заведение.',
     img: mesto,
     site: 'https://r0bomurlok.github.io/react-mesto-auth/',
