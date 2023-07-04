2121b8cca6b05fa7330b55a78b9ffc5b01a8a13f
teplostanski <teplostanski@yandex.ru>
2023-07-05 00:26:19 +0300

    feat: add link conversion methods

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,7 @@ const Index = () => {
       <Head>
         <title>{PageIndexTitle}</title>
         <meta name="keywords" content={keywords} />
-        <meta
-          name="description"
-          content={PageIndexDesc}
-        />
+        <meta name="description" content={PageIndexDesc} />
       </Head>
       <div>Index</div>
     </>
