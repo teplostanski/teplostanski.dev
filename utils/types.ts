e8f13d3abae32e85bdf2918b46f9dd5b8c8dbf5d
teplostanski <teplostanski@yandex.ru>
2023-07-08 16:56:35 +0300

    feat: add AppProps type

--- utils/types.ts
+++ utils/types.ts
@@ -12,7 +12,12 @@ type ProjectsLayout = {
   props?: any
 }
 
+type AppProps = {
+  Component: any
+  pageProps: any
+}
+
 type MainLayoutProps = Children & MainLayout
 type ProjectsLayoutProps = Children & ProjectsLayout
 
-export type { MainLayoutProps, ProjectsLayoutProps }
+export type { MainLayoutProps, ProjectsLayoutProps, AppProps }
