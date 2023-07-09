c642770fa9fc4439e921b169ff5e80fefc745bc9
teplostanski <teplostanski@yandex.ru>
2023-07-09 03:58:33 +0300

    feat: change Main component

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,9 +1,27 @@
+import cn from 'classnames'
 import styles from './Main.module.scss'
 
 const Main = () => {
   return (
     <section className={styles.container}>
-      <h2 className={styles.title}>Main</h2>
+      <div className={cn(styles.container__item, styles.container__item_image)}>
+        <div className={styles.image}></div>
+      </div>
+      <div className={styles.separator}></div>
+      <div className={styles.container__item}>
+        <div className={styles.description}>
+          <div className={styles.description__background}>Dev</div>
+          <div className={styles.description__text}>
+            <span className={styles.description__subtitle}>Привет!</span>
+            <span className={styles.description__subtitle}>
+              Я Игорь Теплостанский
+            </span>
+            <span className={styles.description__title}>
+              Frontend разработчик
+            </span>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
