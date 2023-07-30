c03021cbd70ffbe77d23d0583420ddb87e4d9e82
teplostanski <teplostanski@yandex.ru>
2023-07-30 19:25:18 +0300

    feat: add Header logo

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,10 @@ import styles from './Header.module.scss'
 const Header = () => {
   return (
     <header className={styles.container}>
-      {/*<div>Headergc Хэдер</div>*/}
+      <div className={styles.logo}>
+        <span>portfolio</span>
+        <span className={styles.text}>version 2</span>
+      </div>
     </header>
   )
 }
