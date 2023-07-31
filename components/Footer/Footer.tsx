7631778f55969dcc6e75ebbd75f0f91d8da3ae86
teplostanski <teplostanski@yandex.ru>
2023-08-01 00:13:07 +0300

    feat: add Footer adaptive

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,9 +3,9 @@ import styles from './Footer.module.scss'
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <span className={styles.copyright}>
-        creator Igor Teplostanski &copy; 2022-2023
-      </span>
+      <div className={styles.copyright}>
+        <span>creator Igor Teplostanski</span> <span>&copy; 2022-2023</span>
+      </div>
     </footer>
   )
 }
